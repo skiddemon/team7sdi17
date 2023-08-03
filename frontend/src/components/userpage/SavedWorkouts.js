438d99eab47065c71c2f5742f9d08e41d7d66fcb
skiddemon <dakotaprewitt66@gmail.com>
2023-08-03 01:26:18 -0700

    workouts saved and working out

---
+++ frontend/src/components/userpage/SavedWorkouts.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react"
+import { Card, Button } from "flowbite-react"
+import { useNavigate } from "react-router-dom"
+import Cookies from "js-cookie"
+import NewWorkout from "./newworkout"
+
+
+
+export default function SavedWorkouts ({setSelectedWorkout}) {
+    const Navigate = useNavigate()
+
+const [workoutList, setWorkoutList] = useState([])
+const userid = Cookies.get('userId')
+
+useEffect(() => {
+    fetch(`http://localhost:8080/workoutplan/${userid}`)
+    .then(res => res.json())
+    .then(data => {console.log(data); setWorkoutList(data)})
+    
+}, [])
+
+const WorkoutMapper = () => {
+    const activitiesMapper = (activity) => {
+        console.log(activity)
+        return activity.map( (e) => {
+            return e.sets.length < 2 ? <p>{`${e.exercise_name}:  ${e.sets.length} Set`}</p> : <p>{`${e.exercise_name}:  ${e.sets.length} Sets`}</p>
+        })
+    }
+    return workoutList.map((workout) => {
+        return (
+            <Card onClick={() => {setSelectedWorkout(workout); Navigate('../newWorkout')}}>
+                {workout.name}
+                {activitiesMapper(workout.activity)}
+            </Card>
+        )
+    })
+}
+
+    return (
+        <WorkoutMapper />
+
+    )
+}
